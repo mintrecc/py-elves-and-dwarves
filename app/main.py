47a8ef3fd6b7dbbba8952bd47cbdaaefce43756b
mintrecc <maxdavydenkooo@gmail.com>
2026-08-18 18:52:22 +0300

    fixed elf_ranger and calculate_team_total_rating

--- app/main.py
+++ app/main.py
@@ -4,12 +4,7 @@ from app.players.player import Player
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
-    sum_rating = 0
-
-    for player in players:
-        sum_rating += player.get_rating()
-
-    return sum_rating
+    return sum(player.get_rating() for player in players)
 
 
 def elves_concert(elves: list[Elf]) -> None:
